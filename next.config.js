80c09cc3711cbaf2afc32c7be5469816bd06ed8f
Evgenylogoped <noffewgen@yandex.ru>
2025-10-25 13:23:43 +0300

    CI: build to GHCR on push, stable Dockerfile.prod, Next config ignores; E2E with Postgres; add .dockerignore

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // стабилизируем сборку CSS/минификацию
   swcMinify: false,
   experimental: { optimizeCss: false },
-
-  // главное: не падать на линте и типах при прод-сборке
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
-
 module.exports = nextConfig;
